c3ab7c9d5c9ab731a92580428348e1ea1e63bd5f
Madeline Dutro <dutro.madeline@gmail.com>
2020-06-06 23:32:08 -0400

    login working

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt')
 const router = express.Router();
 const Users = require('../models/users.js');
 
@@ -16,5 +17,17 @@ router.get('/view/:id', (req, res) => {
   res.send("This is where you'd view an individual profile")
 })
 
+router.post('/signup', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
+  Users.create(req.body, (err, createdUser) => {
+    if (err) {
+      console.log(err)
+      res.send('There was an an error - please try to create your account again')
+    } else {
+      console.log('user is created', createdUser)
+      res.redirect('/')
+    }
+  })
+})
 
 module.exports = router
